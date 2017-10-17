09cb3a6a9bab2e50b53cbe203511bd276e9465b3
xxtingxx <xxtingxx@DESKTOP-TUTM66F.home>
2017-10-16 23:33:04 -0200

    .java

--- src/java/br/com/fatecpg/quiz/Usuario.java
+++ src/java/br/com/fatecpg/quiz/Usuario.java
@@ -3,32 +3,14 @@ import java.util.ArrayList;
 
 public class Usuario {
     private String player;
-    private int nota;
-
-    public int getNota() {
-        return nota;
-    }
-
-    public void setNota(int nota) {
-        this.nota = nota;
-    }
-    
+   
     private static ArrayList<Usuario> pessoa;
     public static ArrayList<Usuario> getUsuario(){
         if (pessoa == null){
             pessoa = new ArrayList<>();
         }
         return pessoa;
-    }
     
-    private static ArrayList<Usuario> notaUsuario;
-    public static ArrayList<Usuario> getNotaUsuario(){
-        if (notaUsuario == null){
-            notaUsuario = new ArrayList<>();
-        }
-        return notaUsuario;
-    }
-
     public String getPlayer() {
         return player;
     }
